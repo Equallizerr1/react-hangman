43309261dd9c3fe7c583575549ce68234d091442
Equallizerr1 <samharradine1994@gmail.com>
2024-04-29 14:53:29 +0100

    created basic hangman game

---
+++ src/Hangman.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+
+export default function Hangman({ duration = 120000 }) {
+	const word = "HANGMAN".toUpperCase();
+	const alphabets = [
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+		"K",
+		"L",
+		"M",
+		"N",
+		"O",
+		"P",
+		"Q",
+		"R",
+		"S",
+		"T",
+		"U",
+		"V",
+		"W",
+		"X",
+		"Y",
+		"Z",
+	];
+	const [correctGuesses, setCorrectGuesses] = useState([]);
+	const [timeUp, setTimeUp] = useState(false);
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			setTimeUp(true);
+		}, duration);
+
+		return () => clearTimeout(timeout);
+	}, []);
+
+	const maskedWord = word
+		.split("")
+		.map((letter) => (correctGuesses.includes(letter) ? letter : "_"))
+		.join(" ");
+	return (
+		<div>
+			<p>{maskedWord}</p>
+			{alphabets.map((alphabet, index) => (
+				<button
+					key={index}
+					onClick={() => {
+						if (word.includes(alphabet)) {
+							setCorrectGuesses([...correctGuesses, alphabet]);
+						}
+					}}>
+					{alphabet}
+				</button>
+			))}
+			{timeUp ? <p>You lost!</p> : !maskedWord.includes("_") && <p>You won!</p>}
+		</div>
+	);
+}
